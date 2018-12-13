e3f6aa6abe2a6cbfa333daeaebd8a71b5d3494cd
TayfunKaraderi <karaderitayfun@gmail.com>
2018-12-13 18:26:08 +0000

    Update test_task5.py

--- test_task5.py
+++ test_task5.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -
 # by observation t-slosh for the data trial2.csv should be arround 3
+import task5
 
 def test_task5():
-    Cx, Cy, ts, tslosh = peak('trial2.csv', 20, mv_avN=1)
+    Cx, Cy, ts, tslosh = task5.peak('trial2.csv', 20, mv_avN=1)
     assert  (tslosh < 3.5)
-    assert  (tslosh < 2.5)
+    assert  (tslosh > 2.5)
