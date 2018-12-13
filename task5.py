c95516022527f37965b8f55d43e7b0c07738da1c
TayfunKaraderi <karaderitayfun@gmail.com>
2018-12-13 17:20:25 +0000

    Update task5.py

--- task5.py
+++ task5.py
@@ -11,6 +11,9 @@ import pandas as pd
 
 # read files
 def Read(file_name):
+    """
+    Reads from the the File t, x, y from file
+    """
     
     # read from files
     df = pd.read_csv(file_name, skiprows = 2, index_col=False)
@@ -24,8 +27,7 @@ def Read(file_name):
     yy = [float(i) for i in yy]
     boundary = df['Boundary']
     boundary = [int(i) for i in boundary]
-    
-    
+
     # Remove Boundary particles
     x = []
     y = []
@@ -40,11 +42,9 @@ def Read(file_name):
 
     return  x, y, t
 
-
-
 def t_index(t):
     """
-    input: a list
+    Input: a list
     Return: a list with coordiates where list[i] != list[i]
     """
     dt = np.diff(t)
@@ -62,10 +62,6 @@ def t_index(t):
     
     return store
 
-
-
-
-
 def MVAVARAGE(List, N):
     """
     input:
@@ -73,7 +69,6 @@ def MVAVARAGE(List, N):
         N- N point avarage
     Returns:
         N point avarage of the List
-    
     """
     cumsum, moving_aves = [0], []
     
@@ -87,13 +82,11 @@ def MVAVARAGE(List, N):
     return(moving_aves)
 
 
-def peak(file_name, mv_avN, wallpos):
+def peak(file_name, wallpos, mv_avN=1):
     """
     Inputs: 
         ----------------------------------
-        x - x_coordinates array (exclude walls)
-        y - y_coordinates array (exclude walls)
-        t - t_coordinates array (exclude walls)
+        Filename - Filename as a string
         mv_avN - N move avarage (float or int)
         wallpos - x-coordinates of boundary (float or int)
         ---------------------------------
@@ -115,41 +108,33 @@ def peak(file_name, mv_avN, wallpos):
     
     # tindex = start index of each time
     tindex = t_index(t)
-    #print(tindex)
 
     Crest_height=[]
     Crest_xcoords=[]
     t_sloshing=[]
     for i in range(len(ts)):
-        
-        # Slice x-array, y-array to get a timeframe.
-        gridpointsX = x[tindex[i] : tindex[i+1]]
+
+        # slice x-array, y-array to get a timeframe.
+        gridpointsXX = x[tindex[i] : tindex[i+1]]
         ypoints = y[tindex[i] : tindex[i+1]]
-        gridpointsY = MVAVARAGE(ypoints, mv_avN)
+        gridpointsYY = MVAVARAGE(ypoints, mv_avN)
         
+        #sort x coordinates from low to high and y acordingly
+        gridpointsY = [x for _,x in sorted(zip(gridpointsXX,gridpointsYY))]
+        gridpointsX = np.sort(gridpointsXX)
+        
+        # Find the time index where height is max
         Peak_Indx = np.where(gridpointsY==max(gridpointsY))[0][0]
         Peak = gridpointsY[Peak_Indx]
         Crest_height.append(Peak)
         coord = gridpointsX[Peak_Indx]
         Crest_xcoords.append(coord)
         
-        
-        if coord >= wallpos:
+        if coord >= 0.85*wallpos:
             t_sloshing.append(ts[i])
-            
-        #print(tindex[i])
-        #print(gridpointsX)
-        #print(gridpointsY)
-        #print(t_sloshing)
-        #print(max(gridpointsY), Peak_Indx, coord)
-    #plt.plot(ts, Crest_xcoords, 'o')
-       
-    tslosh = min(t_sloshing)
-    
-    #print("Crest_height", Crest_height)
-    #print("Crest_xcoords", len(Crest_xcoords))
-    #print("tslosh", tslosh)
-    #print("ts", ts)
-    #print(coord)
+    # time to travel 0.85th distce of the domain
+    tslos = min(t_sloshing)
+    # time to travel the distace of the domain
+    tslosh = tslos/0.85
     
     return(Crest_xcoords, Crest_height, ts, tslosh)
